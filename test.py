d5e593a3dd3712cd834732f154a78e29af17b9f3
KempiG <84987806+KempiG@users.noreply.github.com>
2022-09-15 07:08:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
+
+
 
 x = [1,2,3]
 y = [2,3,4]
 df = pd.DataFrame(x,y)
-fig = plt.plot(x,y)
-st.plt(fig=fig)
+plt.plot(x,y)
+st.write(fig)
