785c0087e6096b5fba3ef14954c0ebef79bbb54f
KempiG <84987806+KempiG@users.noreply.github.com>
2022-09-15 07:17:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ import streamlit as st
 
 
 
-x = [1,2,3]
-y = [2,3,4]
+x = [1,2,3,5,1,8,2,4]
+y = [2,3,4,2,5,7,4,2]
 df = pd.DataFrame(x,y)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-plt.plot(df)
+plt.scatter(df)
 plt.title('Dit is een testafbeelding')
 plt.xlabel('x')
 plt.ylabel('y')
