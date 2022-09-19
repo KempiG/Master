965c93511869d47f5a46e90afa3738a0aafa25c7
KempiG <84987806+KempiG@users.noreply.github.com>
2022-09-19 16:28:30 +0200

    Update PVD_funcs.py

--- PVD_funcs.py
+++ PVD_funcs.py
@@ -98,8 +98,8 @@ def show_preview(frame):
                          scale,
                          help='Choose from the list what you want to plot in the figure below', index=8)
      
-    Max_depth_color = frame[frame['Max. depth [m]']>5]['Max. depth [m]']
-    #Max_depth_color = Max_depth_
+    Max_depth_color = frame[frame['Max. depth [m]']>5]
+    Max_depth_color = Max_depth_color['Max.depth [m]']
     st.write(Max_depth_color)
         
     fig = px.scatter(data_frame = frame,
